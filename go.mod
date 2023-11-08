4018d0c18beb32823990774041fb79f150ed0e24
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-07 16:26:12 -0800

    updated version to v0.5.5

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module goki.dev/mobile
 go 1.17
 
 require (
-	golang.org/x/image v0.13.0
-	golang.org/x/mod v0.13.0
-	golang.org/x/sync v0.4.0
+	golang.org/x/image v0.14.0
+	golang.org/x/mod v0.14.0
+	golang.org/x/sync v0.5.0
 	golang.org/x/tools v0.14.0
 )
 
-require golang.org/x/sys v0.13.0 // indirect
+require golang.org/x/sys v0.14.0 // indirect
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package mobile
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.5.4"
+	Version = "v0.5.5"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "28250ee"
+	GitCommit = "7fd1450"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-10-22 17:27"
+	VersionDate = "2023-11-08 00:26"
 )
