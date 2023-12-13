31f1ff0a4a238778a73ab167cdbec06b623b1324
Kai O'Reilly <koreilly5297@gmail.com>
2023-12-12 16:06:47 -0800

    updated version to v0.5.8

--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	golang.org/x/image v0.14.0
 	golang.org/x/mod v0.14.0
 	golang.org/x/sync v0.5.0
-	golang.org/x/tools v0.16.0
+	golang.org/x/tools v0.16.1
 )
--- version.go
+++ version.go
@@ -4,9 +4,9 @@ package mobile
 
 const (
 	// Version is the version of this package being used
-	Version = "v0.5.7"
+	Version = "v0.5.8"
 	// GitCommit is the commit just before the latest version commit
-	GitCommit = "07e21e1"
+	GitCommit = "e9976c6"
 	// VersionDate is the date-time of the latest version commit in UTC (in the format 'YYYY-MM-DD HH:MM', which is the Go format '2006-01-02 15:04')
-	VersionDate = "2023-12-01 19:59"
+	VersionDate = "2023-12-13 00:06"
 )
